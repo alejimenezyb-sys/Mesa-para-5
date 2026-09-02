deeccd92b124bfbc13a0cecda31da416d9465145
Ale Jiménez <alejimenez.yb@gmail.com>
2026-09-02 02:14:45 -0300

    Se crea la carpeta 'interfaces', se crea 'Producto.ts dentro de la misma' con el interface Product, teniendo id, nombre, precio, imagen, categoria, descripcion, y stock

---
+++ src/interfaces/Producto.ts
@@ -0,0 +1,9 @@
+export interface Product {
+    id : string
+    nombre: string
+    precio: number
+    imagen: string
+    categoria: string
+    descripcion: string
+    stock: string
+}
--- src/pages/DetalleProducto.tsx
+++ src/pages/DetalleProducto.tsx
@@ -1,9 +1,12 @@
+import { useParams , Link } from "react-router"
+import { useEffect, useState } from "react"
+import { Product } from "../interfaces/Producto"
 
 
 const DetalleProducto = () => {
   return (
     <div>
-        <h1 className="text-2xl font-bold">Detalle Producto</h1>
+        <h1 className="text-">Detalle Producto</h1>
         <p>mesa para 5</p>
     </div>
   )
