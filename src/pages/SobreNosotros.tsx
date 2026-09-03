b21af6798a500992ab75e9e74b22fb2e42ff67b6
Brandon Yuthiel <monzonbrandon555@gmail.com>
2026-09-03 20:14:19 -0300

    estructura inicial sobre nosotros

--- src/pages/SobreNosotros.tsx
+++ src/pages/SobreNosotros.tsx
@@ -1,10 +1,18 @@
-
 const SobreNosotros = () => {
   return (
-    <div>
-        <h1 className="text-2xl font-bold">Sobre Nosotros</h1>
-        <p>mesa para 5</p>
-    </div>
+    <main>
+      <h1>Acerca de Nosotros</h1>
+
+      <p>
+        Somos un equipo de estudiantes apasionados por la tecnología y el
+        desarrollo web. En este proyecto unimos nuestras ideas y habilidades
+        para crear una tienda online de juegos de mesa.
+      </p>
+
+      <p>“Después lo hacemos”</p>
+
+      <h2>Nuestro equipo</h2>
+    </main>
   )
 }
 
