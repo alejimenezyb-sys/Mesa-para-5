262789e158629bf8ec5363f8377ca30af6c58169
Brandon Yuthiel <monzonbrandon555@gmail.com>
2026-09-03 23:39:30 -0300

    agrego estilos a tarjetas e imagenes de integrantes

--- src/pages/SobreNosotros.tsx
+++ src/pages/SobreNosotros.tsx
@@ -1,4 +1,37 @@
+import monzonbrandon from "../assets/integrantes/monzonbrandon.jpeg"
+import alejimenez from "../assets/integrantes/alejimenez.jpeg"
+import lautaropazzelli from "../assets/integrantes/lautaropazzelli.jpeg"
+import maximoisaias from "../assets/integrantes/maximoisaias.jpeg"
+import guillejimenez from "../assets/integrantes/guillejimenez.jpeg"
+
 const SobreNosotros = () => {
+
+  const integrantes = [
+  {
+    nombre: "Monzon Brandon",
+    imagen: monzonbrandon,
+    posicion: "object-right-top"
+  },
+  {
+    nombre: "Ale Jimenez",
+    imagen: alejimenez
+  },
+  {
+    nombre: "Guille Jimenez",
+    imagen: guillejimenez 
+  },
+  {
+    nombre: "Lautaro Pazzelli",
+    imagen: lautaropazzelli ,
+    posicion: "object-bottom"
+  },
+  {
+    nombre: "Maximo Isaias",
+    imagen: maximoisaias ,
+    posicion: "object-[50%_40%]"
+  }
+];
+
   return (
     <main>
       <h1>Acerca de Nosotros</h1>
@@ -13,27 +46,13 @@ const SobreNosotros = () => {
 
       <h2>Nuestro equipo</h2>
 
-      <div>
-        <article>
-          <img src="" alt="Monzon Brandon" />
-          <h3>Monzon Brandon</h3>
-          </article>
-        <article>
-          <img src="" alt="gAle Jimenez" />
-          <h3>Ale Jimenez</h3>
-        </article>
-        <article>
-          <img src="" alt="Guille Jimenez" />
-          <h3>Guille Jimenez</h3>
-        </article>
-        <article>
-          <img src="" alt="Lautaro Pazzelli" />
-          <h3>Lautaro Pazzelli</h3>
-        </article>
-        <article>
-          <img src="" alt="Maximo Isaias" />
-          <h3>Maximo Isaias</h3>
-        </article>
+      <div className="flex gap-5 flex-wrap justify-center">
+        {integrantes.map((integrante) => (
+        <article className="bg-[#62B6CB] gap-5 rounded-xl p-4 flex items-center flex-col w-52" key={integrante.imagen}>
+        <img className={`w-40 h-40 rounded-full object-cover ${integrante.posicion}`} src={integrante.imagen} />
+       <h3 className="mt-4 text-center font-seminold">{integrante.nombre}</h3>
+  </article>
+        ))}
       </div>
     </main>
   )
