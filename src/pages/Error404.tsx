6707be43600f7558c34f2ff2116264e2496e16d9
Lautaro Pazzelli <lautaropazzelli5@gmail.com>
2026-09-06 23:24:52 -0300

    creación de la base de la pagina de error

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -1,9 +1,21 @@
+import fondoError from '../assets/fondoError.jfif'
 
 const Error404 = () => {
   return (
-    <div>
-        <h1 className="text-2xl font-bold">Error 404</h1>
-        <p>mesa para 5</p>
+    <div className='min-h-screen w-full bg-[#1B4965] flex flex-col items-center justify-center p-4'>
+      <div className="w-full max-w-3xl bg-slate-800 rounded-2xl shadow-2xl overflow-hidden border border-slate-700 flex flex-col items-center text-center p-6 space-y-6">
+      <div className="imagen w-full overflow-hidden rounded-xl bg-slate-950">
+        <img src={fondoError} alt="" className='w-full h-auto max-h-[45vh] object-cover mx-auto'/>
+      
+       <div id="divTexto" className='space-y-2'>
+        <h1 className="text-2xl sm:text-4xl md:text-3xl font-bold text-[#62B6CB]">Error 404: ¡Página No Encontrada! </h1>
+        <p className='text-slate-300 text-sm sm:text-2xl md:text-3xl'>Parece que esta ficha se ha salido del tablero</p>
+       </div>
+       <div className="divBotones w-full sm:w-auto">
+         <a className='inline-block w-full sm:w-auto bg-[#1B4965] hover:bg-amber-600 text-slate-950 font-bold px-6 py-2.5 rounded-xl transition-all shadow-md active:scale-95 text-center mt-2 mb-2'>Volver al Inicio</a>
+       </div>
+      </div>
+      </div>
     </div>
   )
 }
