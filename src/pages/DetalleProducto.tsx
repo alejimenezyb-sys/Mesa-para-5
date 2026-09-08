0aac1b673f84594f67f324f518d0d77945f277df
Ale Jiménez <alejimenez.yb@gmail.com>
2026-09-08 02:19:41 -0300

    refactor(detalle-producto): eliminar importación no utilizada de Link

--- src/pages/DetalleProducto.tsx
+++ src/pages/DetalleProducto.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { useParams , Link } from "react-router"
+import { useParams } from "react-router"
 import type { Juego } from "../interfaces/Producto"
 
 
