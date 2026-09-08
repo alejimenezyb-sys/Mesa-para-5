7e1f28561dbbd311043d088259b59ff0f9acb84f
Ale Jiménez <alejimenez.yb@gmail.com>
2026-09-08 03:04:27 -0300

    refactor(navbar): mapeo de enlaces mediante un array de objetos + se agrega un estilo de la ruta actual/ruta activa

--- src/components/NavBarApp.tsx
+++ src/components/NavBarApp.tsx
@@ -1,12 +1,33 @@
 import { NavLink } from "react-router"
 
 const NavBarApp = () => {
+
+    const ListMenu= [
+        {
+            text: "Inicio",
+            path: "/"
+        },
+        {
+            text: "Sobre Nosotros",
+            path: "sobre"
+        }
+    ]
+
+    
   return (
     <nav className="bg-[#CAE9FF] flex justify-around items-center">
         <div className="text-[#1B4965] font-semibold">
             <ul className="flex gap-4">
-                <li><NavLink>Inicio</NavLink></li>
-                <li>Sobre Nosotros</li>
+                {
+                    ListMenu.map((item , index)=>
+                    
+                        <li key={index}>
+                            <NavLink 
+                                  
+                                 to={item.path} 
+                                 className={({isActive})=> isActive ? " text-blue-900" : "text-[#1B4965]"}>{item.text}</NavLink></li>
+                    )
+                }
             </ul>
         </div>
 
