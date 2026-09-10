9a32d30079120b7d4b34429b1da4de890f96a24f
Ale Jiménez <alejimenez.yb@gmail.com>
2026-09-10 15:38:59 -0300

    refactor(routes): migrar layout a TSX y se añadió el componente navbarapp al approuter

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -3,18 +3,23 @@ import Home from '../pages/Home'
 import DetalleProducto from '../pages/DetalleProducto'
 import SobreNosotros from '../pages/SobreNosotros'
 import Error404 from '../pages/Error404'
-
+import LayoutApp from '../layout/Layout'
 
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route >
+      <Route path="/" element = {<LayoutApp />}>
+        
+
+
         <Route index element = {<Home />}/>
         <Route path= "detalle" element = {<DetalleProducto />}/>
         <Route path= "sobre" element = {<SobreNosotros />}/>  
         <Route path= "*" element = {<Error404 />}/>
-      </Route>
+
+        </Route>
+      
 
     </Routes>
   )
