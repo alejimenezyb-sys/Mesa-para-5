926566566afc6857970579416d5cf7d6e6b2367b
Lautaro Pazzelli <lautaropazzelli5@gmail.com>
2026-09-11 19:17:51 -0300

    Creacion de la base del Login

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -12,7 +12,7 @@ const Error404 = () => {
         <p className='text-slate-300 text-sm sm:text-2xl md:text-3xl'>Parece que esta ficha se ha salido del tablero</p>
        </div>
        <div className="divBotones w-full sm:w-auto">
-         <a className='inline-block w-full sm:w-auto bg-[#1B4965] hover:bg-amber-600 text-slate-950 font-bold px-6 py-2.5 rounded-xl transition-all shadow-md active:scale-95 text-center mt-2 mb-2'>Volver al Inicio</a>
+         <a className='cursor-pointer inline-block w-full sm:w-auto bg-[#1B4965] hover:bg-amber-600 text-slate-950 font-bold px-6 py-2.5 rounded-xl transition-all shadow-md active:scale-95 text-center mt-2 mb-2'>Volver al Inicio</a>
        </div>
       </div>
       </div>
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -2,6 +2,7 @@ import { Routes , Route } from 'react-router'
 import Home from '../pages/Home'
 import DetalleProducto from '../pages/DetalleProducto'
 import SobreNosotros from '../pages/SobreNosotros'
+import Login from '../pages/LoginPanel'
 import Error404 from '../pages/Error404'
 
 
@@ -13,6 +14,7 @@ const AppRouter = () => {
         <Route index element = {<Home />}/>
         <Route path= "detalle" element = {<DetalleProducto />}/>
         <Route path= "sobre" element = {<SobreNosotros />}/>  
+        <Route path="Login" element={<Login />} />
         <Route path= "*" element = {<Error404 />}/>
       </Route>
 
