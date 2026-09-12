1a9744fa6a418cf749749d7c4021425d49f925ab
Ale Jiménez <alejimenez.yb@gmail.com>
2026-09-12 15:50:49 -0300

    fix: resolver conflicto de merge en AppRouter

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import DetalleProducto from "../pages/DetalleProducto";
-import SobreNosotros from "../pages/SobreNosotros";
-import Error404 from "../pages/Error404";
-=======
 import { Routes , Route } from 'react-router'
 import Home from '../pages/Home'
 import DetalleProducto from '../pages/DetalleProducto'
@@ -14,19 +7,10 @@ import LayoutApp from '../layout/Layout'
 import AdminPanel from '../pages/AdminPanel'
 
 
->>>>>>> footer
 
 const AppRouter = () => {
   return (
     <Routes>
-<<<<<<< HEAD
-      <Route>
-        <Route index element={<Home />} />
-        <Route path="detalle" element={<DetalleProducto />} />
-        <Route path="sobre" element={<SobreNosotros />} />
-        <Route path="*" element={<Error404 />} />
-      </Route>
-=======
       <Route path="/" element = {<LayoutApp />}>
         
 
@@ -41,7 +25,6 @@ const AppRouter = () => {
       
       
 
->>>>>>> footer
     </Routes>
   );
 };
