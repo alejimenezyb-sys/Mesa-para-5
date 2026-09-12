97e516f3cb7f94485e1d1600954d9714d000a8a4
guillermo jimenez <barbillajimenez08@gmail.com>
2026-09-12 13:44:26 -0300

    feat: create homepage with product catalog and filters

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,23 +1,20 @@
-import { Routes , Route } from 'react-router'
-import Home from '../pages/Home'
-import DetalleProducto from '../pages/DetalleProducto'
-import SobreNosotros from '../pages/SobreNosotros'
-import Error404 from '../pages/Error404'
-
-
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import DetalleProducto from "../pages/DetalleProducto";
+import SobreNosotros from "../pages/SobreNosotros";
+import Error404 from "../pages/Error404";
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route >
-        <Route index element = {<Home />}/>
-        <Route path= "detalle" element = {<DetalleProducto />}/>
-        <Route path= "sobre" element = {<SobreNosotros />}/>  
-        <Route path= "*" element = {<Error404 />}/>
+      <Route>
+        <Route index element={<Home />} />
+        <Route path="detalle" element={<DetalleProducto />} />
+        <Route path="sobre" element={<SobreNosotros />} />
+        <Route path="*" element={<Error404 />} />
       </Route>
-
     </Routes>
-  )
-}
+  );
+};
 
-export default AppRouter
+export default AppRouter;
