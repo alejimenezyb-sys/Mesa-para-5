612f8028181c3ef899e85f14d2bed24af5bef7e2
Brandon Yuthiel <monzonbrandon555@gmail.com>
2026-09-13 18:03:40 -0300

    feat: agrego validaciones al login

--- src/pages/LoginPanel.tsx
+++ src/pages/LoginPanel.tsx
@@ -1,8 +1,27 @@
-import React, { useState } from 'react';
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
 
-const emailRegexp = new RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
-
+type DatosLogin = {
+  correo: string 
+  contraseña: string
+} 
 const Login = () => {
+
+  const { register,
+   handleSubmit,
+    formState: { errors } 
+  } = useForm<DatosLogin>();
+  const navigate = useNavigate() 
+
+    const iniciarSesion = (data: DatosLogin) => {
+      console.log(data.correo)
+      console.log(data.contraseña)
+
+      navigate("/adminpanel")
+     }
+
+     
+
   return (
     <div className="min-h-screen bg-[#CAE9FF] flex items-center justify-center p-4">
       <div className="w-full max-w-md bg-[#1B4965] rounded-2xl shadow-2xl p-8 space-y-6">
@@ -15,17 +34,31 @@ const Login = () => {
           </h2>
         </div>
 
-        <form className="space-y-4">
+        <form
+        noValidate
+        onSubmit={handleSubmit(iniciarSesion)} className="space-y-4">
           <div className="space-y-1">
             <label className="block text-sm font-semibold text-[#CAE9FF]">
               Correo
             </label>
             <input
               type="email"
-              name="correo"
-              placeholder="ejemplo@gmail.com"
-              className="w-full px-4 py-2.5 bg-white/90 border border-[#62B6CB] rounded-lg text-[#1B4965] placeholder-[#5FA8D3] focus:outline-none focus:ring-2 focus:ring-[#1B4965] focus:bg-white transition"
+              { ...register("correo", { 
+                required: "El correo es obligatorio",
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "El correo no es valido"
+                 } 
+                } ) }
+              
+              placeholder="example@gmail.com"
+              className={`w-full px-4 py-2.5 bg-white/90 border border-[#62B6CB] rounded-lg text-[#1B4965] placeholder-[#5FA8D3] focus:outline-none focus:ring-2 focus:ring-[#1B4965] focus:bg-white transition ${errors.contraseña ? "border-red-500" : "border-gray-300"}`}
             />
+            {errors.correo && (
+          <p className="text-red-500">
+            {errors.correo.message}
+          </p>
+        )}
           </div>
 
           <div className="space-y-1">
@@ -34,11 +67,23 @@ const Login = () => {
             </label>
             <input
               type="password"
-              name="contraseña"
-              maxLength={8}
+               { ...register("contraseña", {
+                required: "La contraseña es obligatoria",
+                minLength: {
+                  value: 8,
+                  message: "La contraseña debe tener al menos 8 caracteres"
+                }
+               })}
               placeholder="********"
-              className="w-full px-4 py-2.5 bg-white/90 border border-[#62B6CB] rounded-lg text-[#1B4965] placeholder-[#5FA8D3] focus:outline-none focus:ring-2 focus:ring-[#1B4965] focus:bg-white transition"
+              className={`w-full px-4 py-2.5 bg-white/90 border border-[#62B6CB] rounded-lg text-[#1B4965] placeholder-[#5FA8D3] focus:outline-none focus:ring-2 focus:ring-[#1B4965] focus:bg-white transition
+                 ${errors.contraseña ? "border-red-500" : "border-gray-300"}`}
             />
+
+               {errors.contraseña && (
+          <p className="text-red-500">
+            {errors.contraseña.message}
+          </p>
+            )}
           </div>
 
           <button
