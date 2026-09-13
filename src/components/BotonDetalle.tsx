0208146ce01cfa5992d8420a874ceb10b9bc720b
isaias lescano <isaias123lescano@gmail.com>
2026-09-13 20:48:20 -0300

    style: mejorar boton de detalle

--- src/components/BotonDetalle.tsx
+++ src/components/BotonDetalle.tsx
@@ -1,12 +1,14 @@
-import { Link } from "react-router"
+import { Link } from "react-router";
 
 const BotonDetalle = () => {
   return (
-    <Link to ="/detalle"
-    className="w-full bg-[#1B4965] ">
-    Ver Detalle / Comprar
+    <Link
+      to="/detalle"
+      className="block w-full rounded-lg bg-[#1B4965] px-4 py-3 text-center font-semibold text-white shadow-md transition duration-200 hover:bg-[#163D54] hover:shadow-lg"
+    >
+      Ver Detalle / Comprar
     </Link>
-  )
-}
+  );
+};
 
-export default BotonDetalle
+export default BotonDetalle;
