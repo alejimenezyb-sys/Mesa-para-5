217d48afde38002c80e979cbe28057a473e88e6a
Ale Jiménez <alejimenez.yb@gmail.com>
2026-09-13 21:24:09 -0300

    feat: se crea el componente AdminRoute para que funcione como componente padre para la route de adminpanel y login, así teniendo las rutas protegidas

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -6,6 +6,7 @@ import Login from '../pages/LoginPanel'
 import Error404 from '../pages/Error404'
 import LayoutApp from '../layout/Layout'
 import AdminPanel from '../pages/AdminPanel'
+import AdminRoute from '../components/AdminRoute'
 
 
 
@@ -19,8 +20,14 @@ const AppRouter = () => {
         <Route index element = {<Home />}/>
         <Route path= "detalle" element = {<DetalleProducto />}/>
         <Route path= "sobre" element = {<SobreNosotros />}/>  
+
+        <Route element={<AdminRoute/>}>
         <Route path="login" element={<Login />} />
         <Route path="adminpanel" element={<AdminPanel />} />
+
+        </Route>
+
+
         <Route path= "*" element = {<Error404 />}/>
 
         </Route>
