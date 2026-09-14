d05d0709692499537637ec293f7dfe987775a6b5
Ale Jiménez <alejimenez.yb@gmail.com>
2026-09-13 21:09:00 -0300

    refactor: BotonDetalle lleva a 404

--- src/components/BotonDetalle.tsx
+++ src/components/BotonDetalle.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 const BotonDetalle = () => {
   return (
     <Link
-      to="/detalle"
+      to="/404"
       className="block w-full rounded-lg bg-[#1B4965] px-4 py-3 text-center font-semibold text-white shadow-md transition duration-200 hover:bg-[#163D54] hover:shadow-lg"
     >
       Ver Detalle / Comprar
